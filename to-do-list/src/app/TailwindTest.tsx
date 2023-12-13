5b6895474f60a90f8de5e2f0fe0c86ecc9419258
5erenah <shua7220@uni.sydney.edu.au>
2023-12-13 16:20:40 +1100

    testing tailwind

---
+++ to-do-list/src/app/TailwindTest.tsx
@@ -0,0 +1,10 @@
+const TailwindTest = () => {
+    return (
+      <div className="p-4 mb-4 text-white bg-blue-500">
+        If you see this text with padding and a blue background, Tailwind is working!
+      </div>
+    );
+  };
+  
+  export default TailwindTest;
+  
